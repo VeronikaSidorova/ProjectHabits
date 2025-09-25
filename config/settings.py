cf7369fd8339e592ad3e8b161ad2e4bb02c4937c
Veronika Sidorova <veronikamartyn2106@yandex.ru>
2025-09-25 14:40:00 +0300

    ci test

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY") or "jchdjdhjhjd"
 
 DEBUG = True
 
